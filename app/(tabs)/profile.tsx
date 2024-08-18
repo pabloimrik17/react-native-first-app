e72de10f4d1dfdddc2c04d10e6e599e842ac00fe
Etherless <pabloimrik17@gmail.com>
2024-08-18 16:26:30 +0200

    fix broken build

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,16 +1,12 @@
 import { View, Text } from "react-native";
 import React, { FC } from "react";
-import ParallaxScrollView from "@/components/ParallaxScrollView";
-import { ThemedView } from "@/components/ThemedView";
-import { ThemedText } from "@/components/ThemedText";
-import Ionicons from "@expo/vector-icons/Ionicons";
 
- export default function Profile ()  {
+export default function Profile() {
     return (
-        <ParallaxScrollView headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
-                            headerImage={<Ionicons size={310} name="code-slash"/>}>
-            <ThemedView>
-                <ThemedText>Profile</ThemedText></ThemedView></ParallaxScrollView>
+        <View>
+            <Text> Profile </Text>
+        </View>
+
     )
 }
 

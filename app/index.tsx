685c499f9deee3f9af97574719b30c20f8d09664
Etherless <pabloimrik17@gmail.com>
2024-08-20 20:49:36 +0200

    added index page

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,61 @@
-import { Link } from "expo-router";
+import { CustomButton } from "@/components/custom-button";
+import { images } from "@/constants";
+import { tailwindResolvedConfig } from "@/tailwind-resolved-config";
+import { router } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { FC } from "react";
-import { Text, View } from "react-native";
+import { Image, ScrollView, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-export const App: FC = () => {
+const App: FC = () => {
   return (
-    <View className={"flex-1 items-center justify-center bg-white"}>
-      <Text className={"text-3xl font-pblack"}>Aora!</Text>
-      <StatusBar style={"auto"} />
-      <Link href={"/profile"} style={{ color: "blue" }}>
-        Go to Profile
-      </Link>
-    </View>
+    <SafeAreaView className={"bg-primary h-full"}>
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className={"w-full justify-center items-center h-[85vh] px-4"}>
+          <Image
+            source={images.logo}
+            className={"w-[130px] h-[84px]"}
+            resizeMode={"contain"}
+          />
+          <Image
+            source={images.cards}
+            className={"max-w--[380px] w-full h-[300px]"}
+            resizeMode={"contain"}
+          />
+          <View className={"relative mt-5"}>
+            <Text className={"text-3xl text-white font-bold text-center"}>
+              Discover Endless Possibilietes with{" "}
+              <Text className={"text-secondary-200"}>Aora</Text>
+            </Text>
+            <Image
+              source={images.path}
+              className={"w-[136px] h-[15px] absolute -bottom-2 -right-8"}
+              resizeMode={"contain"}
+            />
+          </View>
+          <Text
+            className={"text-sm font-pregular text-gray-100 mt-7 text-center"}
+          >
+            Where creativity meets innovation: embark on a journey of limitless
+            exploration with Aora
+          </Text>
+          <CustomButton
+            onPress={() => router.push("/sign-in")}
+            classNames={{
+              container: "w-full mt-7",
+            }}
+          >
+            Continue with email
+          </CustomButton>
+        </View>
+      </ScrollView>
+      <StatusBar
+        backgroundColor={
+          tailwindResolvedConfig?.theme?.colors?.primary as string
+        }
+        style={"light"}
+      />
+    </SafeAreaView>
   );
 };
 

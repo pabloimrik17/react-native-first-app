18c23c47d741c41670e29013cb794d909ceff8ce
Etherless <pabloimrik17@gmail.com>
2024-08-20 20:11:03 +0200

    added prettier

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  plugins: ["prettier-plugin-organize-imports"],
+};
+
+module.exports = config;

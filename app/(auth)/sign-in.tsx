678b67faa856cf13482932ed768c329f8b93e981
Pablo F. Guerra <pabloimrik17@gmail.com>
2024-08-24 18:54:39 +0200

    Added react hook form, zod and first iteration of sign in page

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,11 +1,44 @@
+import { images } from "@/constants";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { FC } from "react";
-import { Text, View } from "react-native";
+import { useForm } from "react-hook-form";
+import { Image, ScrollView, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { z } from "zod";
+
+const SignInFormValues = z.object({
+  email: z.string().email(),
+  password: z.string().trim().min(5),
+});
+
+type SignInFormValues = z.infer<typeof SignInFormValues>;
 
 const SignIn: FC = () => {
+  const {} = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+    resolver: zodResolver(SignInFormValues),
+  });
+
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView className={"bg-primary h-full"}>
+      <ScrollView>
+        <View className={"w-full justify-center h-full px-4 my-6"}>
+          <Image
+            source={images.logo}
+            resizeMode={"contain"}
+            className={"w-[115px] h-[35px]"}
+          />
+          <Text
+            className={"text-2xl text-white text-semibold mt-10 font-psemibold"}
+          >
+            Log in to Aora
+          </Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
---
+++ components/form-field.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+import { Text, View } from "react-native";
+
+export const FormField: FC = (props) => {
+  return (
+    <View className={"space-y-2"}>
+      <Text className={"text-base text-gray-100 font-psemibold"}> </Text>
+    </View>
+  );
+};
